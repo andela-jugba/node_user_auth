0420e935b67181b8207b6f6d7bc52015c1a76869
Joshua Ugba <justjosh001@gmail.com>
2017-08-10 06:00:53 +0100

    Adds passport for user authentication

--- models/user.js
+++ models/user.js
@@ -28,9 +28,27 @@ const UserSchema = mongoose.Schema({
 
 const User = module.exports = mongoose.model('User', UserSchema);
 
+module.exports.getUserById = function(id, callback) {
+  User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback) {
+  let query = {username: username};
+  User.findOne(query, callback);
+}
+
+module.exports.comparePassword = function(candidatePasword, hash, callback){
+  bcrypt.compare(candidatePasword, hash, function(err,isMatch) {
+    console.log('candidatePasword',candidatePasword);
+    console.log('Hasd',hash);
+    console.log('Match', isMatch);
+   callback(null, isMatch);
+});
+}
+
 module.exports.creatUser = function(newUser, callback) {
   bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash("newUser.password", salt, function(err, hash) {
+    bcrypt.hash(newUser.password, salt, function(err, hash) {
       newUser.password = hash;
       newUser.save(callback);
     });
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
-  const express = require('express');
+const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const upload = multer({dest: './uploads'});
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const User = require('../models/user');
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function(username, password, done){
+  User.getUserByUsername(username, function(err, user){
+    if (err) throw err;
+    if(!user){
+      return done(null, false, {message: 'Unknown User'});
+    }
+    User.comparePassword(password, user.password, function (err, isMatch) {
+      if(err) return done(err);
+      console.log('Match', isMatch);
+      if(isMatch){
+      return done(null, user);
+      } else {
+        console.log('Here!')
+      return done(null, false, {message: 'Invalid Password'});
+    }
+    })
+  })
+}));
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -63,5 +94,16 @@ router.post('/register', upload.single('profileimage'), function(req, res, next)
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
+router.post('/login', passport.authenticate('local', {failureRedirect: '/users/login', failureFlash: 'Invalid Username or password'}), function(req, res, next) {
+  // If this function gets called, authentication was successful.
+   // `req.user` contains the authenticated user.
+   req.flash('succces', 'you are now logged in')
+   res.redirect('/');
+});
 
+router.get('/logout', function(req, res) {
+  req.logout();
+  req.flash('succces', 'You are now logged out');
+  res.redirect('/users/login');
+});
 module.exports = router;
